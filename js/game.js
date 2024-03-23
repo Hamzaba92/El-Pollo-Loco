e98ef999c8209a5ffe4064c82f7093f9d118ec1a
Hamzaba92 <hamzabaj1992@gmail.com>
2024-03-23 04:04:29 +0100

    fix music bugs

--- js/game.js
+++ js/game.js
@@ -8,53 +8,75 @@ function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
     console.log('My Character is', world.character);
+    playBackgroundMusic();
 }
 
 window.addEventListener('keydown', (e) => {
-    if (e.keyCode == 39){
+    if (e.keyCode == 39) {
         keyboard.RIGHT = true;
     }
 
-    if (e.keyCode == 37){
+    if (e.keyCode == 37) {
         keyboard.LEFT = true;
     }
 
-    if (e.keyCode == 38){
+    if (e.keyCode == 38) {
         keyboard.UP = true;
     }
 
-    if (e.keyCode == 40){
+    if (e.keyCode == 40) {
         keyboard.DOWN = true;
     }
 
-    if (e.keyCode == 32){
+    if (e.keyCode == 32) {
         keyboard.SPACE = true;
     }
 
-    
-       
+
+
 });
 
 window.addEventListener('keyup', (e) => {
-    if (e.keyCode == 39){
+    if (e.keyCode == 39) {
         keyboard.RIGHT = false;
     }
 
-    if (e.keyCode == 37){
+    if (e.keyCode == 37) {
         keyboard.LEFT = false;
     }
 
-    if (e.keyCode == 38){
+    if (e.keyCode == 38) {
         keyboard.UP = false;
     }
 
-    if (e.keyCode == 40){
+    if (e.keyCode == 40) {
         keyboard.DOWN = false;
     }
 
-    if (e.keyCode == 32){
+    if (e.keyCode == 32) {
         keyboard.SPACE = false;
     }
-        
+
 
 })
+
+
+let toggle = true;
+
+
+function playBackgroundMusic() {
+
+    let toggleImg = document.getElementById('toggle_sound_btn');
+    BACKGROUND_SOUND.loop = true;
+    if (toggle) {
+        BACKGROUND_SOUND.play();
+        toggleImg.src = './img/sound_on.png';
+    } else {
+        BACKGROUND_SOUND.pause();
+        toggleImg.src = './img/sound_mute.png';
+    }
+
+    toggle = !toggle;
+}
+
+
--- js/sounds.js
+++ js/sounds.js
@@ -1,8 +1,7 @@
 
 let soundActive = false;
 
-WALKING_SOUND = new Audio('audio/walking_pepe.mp3');
-JUMPING_SOUND = new Audio('audio/pepe_jump.mp3');
+
 BACKGROUND_SOUND = new Audio('audio/pepe_background_sound2.mp3');
 
 
